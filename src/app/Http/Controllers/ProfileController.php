868c63f23fe1fc0efe5b806806d66593f5281b8a
igl112 <igl112@inlumine.ual.es>
2025-03-11 12:31:08 +0100

    Manejo de errores en Login, Register y ProfileController

--- src/app/Http/Controllers/ProfileController.php
+++ src/app/Http/Controllers/ProfileController.php
@@ -25,11 +25,32 @@ class ProfileController extends Controller
         // Validar los datos de entrada
         $validatedData = $request->validate([
             'apellidos' => 'required|string|max:255',
-            'tlf' => 'required|string|max:20',
+            'tlf' => 'required|digits_between:9,15|regex:/^\+?\d+$/',
             'direccion' => 'required|string|max:255',
             'municipio' => 'required|string|max:255',
             'provincia' => 'required|string|max:255',
-            'DNI' => 'required|string|max:9',
+            'DNI' => 'required|string|size:9|unique:clientes,DNI|regex:/^\d{8}[A-Z]$/',
+        ], [
+            'apellidos.string' => 'Los apellidos deben ser una cadena de texto.',
+            'apellidos.required' => 'Los apellidos son obligatorios.',
+
+            'tlf.digits_between' => 'El número de teléfono tiene que ser una cadena de dígitos.' ,
+            'tlf.required' => 'El número de teléfono es obligatorio',
+            'tlf.regex' => 'El número de teléfono sólo puede tener números y opcionalmente +.',
+
+            'direccion.string' => 'La direccion debe ser una cadena de texto.',
+            'direccion.required' => 'La direccion es obligatorio.',
+
+            'municipio.string' => 'El municipio debe ser una cadena de texto.',
+            'municipio.required' => 'El municipio es obligatorio.',
+
+            'provincia.string' => 'La provincia debe ser una cadena de texto.',
+            'provincia.required' => 'La provincia es obligatorio.',
+
+            'DNI.required' => 'El DNI es obligatorio.',
+            'DNI.size' => 'El DNI debe tener 9 caracteres.',
+            'DNI.regex' => 'El formato del DNI no es válido. Debe tener 8 números seguidos de una letra.',
+            'DNI.unique' => 'El DNI ya está registrado en el sistema.',
         ]);
 
         // Crear el cliente y asociarlo con el usuario
@@ -75,16 +96,38 @@ class ProfileController extends Controller
             'nombre' => 'sometimes|required|string|max:255',
             'nombreUsuario' => 'sometimes|required|string|max:255|unique:usuarios,nombreUsuario,' . $user->id,
             'email' => 'sometimes|required|email|unique:usuarios,email,' . $user->id,
+        ], [
+            'nombre.string' => 'El nombre tiene que ser una cadena de texto.',
+
+            'nombreUsuario.unique' => 'El nombre de usuario ya está en uso.',
+
+            'email.email' => 'El email debe ser una dirección válida.',
+            'email.unique' => 'El email ya está en uso.',
         ]);
 
         // Validar los datos para el cliente
         $validatedClientData = $request->validate([
             'apellidos' => 'sometimes|required|string|max:255',
-            'tlf' => 'sometimes|required|string|max:20',
+            'tlf' => 'sometimes|required|digits_between:9,15|regex:/^\+?\d+$/',
             'direccion' => 'sometimes|required|string|max:255',
             'municipio' => 'sometimes|required|string|max:255',
             'provincia' => 'sometimes|required|string|max:255',
-            'DNI' => 'sometimes|required|string|max:9',
+            'DNI' => 'sometimes|required|string|size:9|unique:clientes,DNI|regex:/^\d{8}[A-Z]$/',
+        ], [
+            'apellidos.string' => 'Los apellidos deben ser una cadena de texto.',
+
+            'tlf.digits_between' => 'El número de teléfono tiene que ser una cadena de dígitos entre 9 y 15.' ,
+            'tlf.regex' => 'El número de teléfono sólo puede tener números y opcionalmente +.',
+
+            'direccion.string' => 'La direccion debe ser una cadena de texto.',
+
+            'municipio.string' => 'El municipio debe ser una cadena de texto.',
+
+            'provincia.string' => 'La provincia debe ser una cadena de texto.',
+
+            'DNI.size' => 'El DNI debe tener 9 caracteres.',
+            'DNI.regex' => 'El formato del DNI no es válido. Debe tener 8 números seguidos de una letra.',
+            'DNI.unique' => 'El DNI ya está registrado en el sistema.',
         ]);
 
         $user->update($validatedUserData);
